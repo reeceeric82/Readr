d6fa4658f0e0b4672df4fe90b5df68961e6c88b6
Reece Russell <rerussell@protonmail.com>
2023-11-08 12:31:11 +0200

    Completed  - Full basic functionality

--- app.js
+++ app.js
@@ -2,25 +2,32 @@ let textChangeTimer;
 let isPaused = false;
 let currentIndex = 0;
 const words = ["This", "is", "the", "program", "running", "."];
+let userText = [];
+let text;
+let wmp = 220;
 
 const textItem = document.getElementById("text-item");
 const startButton = document.getElementById("start-btn");
 const stopButton = document.getElementById("stop-btn");
 const pauseButton = document.getElementById("pause-btn");
+// const submitButton = document.getElementById("submit-btn");
 
-// write loop through array of text
+// Functions
+const getUserText = () => {
+  text = document.getElementById("user-input").value;
+  userText = text.split(" ");
+};
+
 const changeText = () => {
-  textItem.textContent = words[currentIndex];
-  currentIndex = (currentIndex + 1) % words.length;
-
-  if (currentIndex === 0) {
-    stopTextChange("Completed");
-  }
+  textItem.textContent = userText[currentIndex];
+  currentIndex = (currentIndex + 1) % userText.length;
 };
 
 const startTextChange = () => {
+  getUserText();
+  wmp = document.getElementById("wpm").value;
   if (!textChangeTimer) {
-    textChangeTimer = setInterval(changeText, 500);
+    textChangeTimer = setInterval(changeText, (60 / parseFloat(wmp)) * 1000);
   }
 };
 
@@ -34,14 +41,16 @@ const pauseTextChange = () => {
   }
 };
 
-const stopTextChange = (textDisplayed = "Stopped") => {
+const stopTextChange = () => {
   clearInterval(textChangeTimer);
-  textItem.textContent = textDisplayed;
+  textItem.textContent = "Stopped";
   textChangeTimer = null;
   currentIndex = 0;
   isPaused = false;
 };
 
+// Event Listeners
 startButton.addEventListener("click", startTextChange);
 pauseButton.addEventListener("click", pauseTextChange);
 stopButton.addEventListener("click", stopTextChange);
+// submitButton.addEventListener("click", getUserText);
